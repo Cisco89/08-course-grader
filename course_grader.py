52af6dcefa02196df99f011b78c93748d6d8c8d7
Francisco Adrian Garcia <franciscoadriangarcia1@gmail.com>
2019-04-23 13:42:36 -0600

    Update course_grader.py

--- course_grader.py
+++ course_grader.py
@@ -1,2 +1,15 @@
 def course_grader(param):
-    return ""
+    
+    pass_fail = "fail"
+
+    for number in scores:
+        if number < 50:
+            return pass_fail
+        
+    average = sum(scores) / len(scores)
+    
+    if average >= 70:
+        pass_fail = "pass"
+        return pass_fail
+    
+    return pass_fail
